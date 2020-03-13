bfeb81de70b772f9e7574064dc426f6506753506
kinghuin <kinghuin_chull@163.com>
2020-03-13 18:29:41 +0800

    debug

--- modules/text/semantic_model/bert_multi_uncased_L_12_H_768_A_12/download.sh
+++ modules/text/semantic_model/bert_multi_uncased_L_12_H_768_A_12/download.sh
@@ -2,7 +2,7 @@ rawname=$1
 
 wget https://bert-models.bj.bcebos.com/multilingual_L-12_H-768_A-12.tar.gz --no-check-certificate
 tar xf multilingual_L-12_H-768_A-12.tar.gz
-mv multilingual_L-12_H-768_A-12 assets $rawname/assets
+mv multilingual_L-12_H-768_A-12 $rawname/assets
 rm multilingual_L-12_H-768_A-12.tar.gz
 
 cd $rawname/assets/params

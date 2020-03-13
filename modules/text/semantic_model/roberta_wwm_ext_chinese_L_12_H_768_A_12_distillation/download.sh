958e859f4d2435b698c93e70694340adae8d3f1c
kinghuin <kinghuin_chull@163.com>
2020-03-13 17:05:45 +0800

    add $

--- modules/text/semantic_model/roberta_wwm_ext_chinese_L_12_H_768_A_12_distillation/download.sh
+++ modules/text/semantic_model/roberta_wwm_ext_chinese_L_12_H_768_A_12_distillation/download.sh
@@ -6,7 +6,7 @@ rm chinese_rbt3_L-3_H-768_A-12_fluid.tar.gz
 rawname=$1
 name=${rawname//L_12_H_768_A_12/L-12_H-768_A-12}
 name=${name//L_24_H_1024_A_16/L-24_H-1024_A-16}
-cd rawname/assets/params
+cd $rawname/assets/params
 for f in * ; do mv "$f" "@HUB_$name@$f"; done
 
 cd -

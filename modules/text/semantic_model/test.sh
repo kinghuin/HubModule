ea32198ff95e6dab3f5e7be1dc7f20b86e4a194c
kinghuin <kinghuin_chull@163.com>
2020-03-23 15:28:01 +0800

    rename bertwwm

--- modules/text/semantic_model/test.sh
+++ modules/text/semantic_model/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for name in roberta_wwm_ext_chinese_L_3_H_768_A_12 roberta_wwm_ext_chinese_L_3_H_1024_A_16 \
+for name in rbt3 rbtl3 \
 bert_cased_L_12_H_768_A_12 bert_cased_L_24_H_1024_A_16 bert_chinese_L_12_H_768_A_12 bert_multi_cased_L_12_H_768_A_12 \
-bert_multi_uncased_L_12_H_768_A_12 bert_uncased_L_12_H_768_A_12 bert_uncased_L_24_H_1024_A_16 bert_wwm_chinese_L_12_H_768_A_12 \
-bert_wwm_ext_chinese_L_12_H_768_A_12 ernie ernie_tiny ernie_v2_eng_base ernie_v2_eng_large roberta_wwm_ext_chinese_L_12_H_768_A_12 \
-roberta_wwm_ext_chinese_L_24_H_1024_A_16
+bert_multi_uncased_L_12_H_768_A_12 bert_uncased_L_12_H_768_A_12 bert_uncased_L_24_H_1024_A_16 chinese_bert_wwm \
+chinese_bert_wwm_ext ernie ernie_tiny ernie_v2_eng_base ernie_v2_eng_large chinese_roberta_wwm_ext \
+chinese-roberta-wwm-ext-large
   do
       python test_score_time.py --name $name
   done
